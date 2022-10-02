44db6db56ce3738e5efb61cb9760c49362053535
ANUSH <109873610+anushhdev@users.noreply.github.com>
2022-10-02 14:33:22 +0530

    changeinimportmaterialdart

--- landpage/lib/main.dart
+++ landpage/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_web/material.dart';
+import "package:flutter/material.dart";
 import 'widgets/navbar.dart';
 import 'utils/responsiveLayout.dart';
 import 'widgets/search.dart';
