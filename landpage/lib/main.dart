acb77b078fc7653be0fb4934620b4408344ad0f1
ANUSH <109873610+anushhdev@users.noreply.github.com>
2022-10-02 14:27:23 +0530

    pagedecorative

--- landpage/lib/main.dart
+++ landpage/lib/main.dart
@@ -1,115 +1,162 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_web/material.dart';
+import 'widgets/navbar.dart';
+import 'utils/responsiveLayout.dart';
+import 'widgets/search.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+void main() => runApp(MaterialApp(
+      title: 'Flutter Landing Page',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
+    ));
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+        Color(0xFFF8FBFF),
+        Color(0xFFFCFDFD),
+      ])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[NavBar(), Body()],
+          ),
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return ResponsiveLayout(
+      largeScreen: LargeChild(),
+      smallScreen: SmallChild(),
+    );
+  }
+}
+
+class LargeChild extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 600,
+      child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          FractionallySizedBox(
+            alignment: Alignment.centerRight,
+            widthFactor: .6,
+            child: Image.network("assets/image_01.png", scale: .85),
+          ),
+          FractionallySizedBox(
+            alignment: Alignment.centerLeft,
+            widthFactor: .6,
+            child: Padding(
+              padding: EdgeInsets.only(left: 48),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Hello!",
+                      style: TextStyle(
+                          fontSize: 60,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Montserrat-Regular",
+                          color: Color(0xFF8591B0))),
+                  RichText(
+                    text: TextSpan(
+                        text: "WellCome To ",
+                        style:
+                            TextStyle(fontSize: 60, color: Color(0xFF8591B0)),
+                        children: [
+                          TextSpan(
+                              text: "Britu",
+                              style: TextStyle(
+                                  fontSize: 60,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black87))
+                        ]),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12.0, top: 20),
+                    child: Text("LET’S EXPLORE THE WORLD"),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Search()
+                ],
+              ),
+            ),
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    );
+  }
+}
+
+class SmallChild extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(40),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              "Hello!",
+              style: TextStyle(
+                  fontSize: 40,
+                  color: Color(0xFF8591B0),
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Montserrat-Regular"),
             ),
+            RichText(
+              text: TextSpan(
+                text: 'WellCome To ',
+                style: TextStyle(fontSize: 40, color: Color(0xFF8591B0)),
+                children: <TextSpan>[
+                  TextSpan(
+                      text: 'Britu',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40,
+                          color: Colors.black87)),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 12.0, top: 20),
+              child: Text("LET’S EXPLORE THE WORLD"),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Center(
+              child: Image.network(
+                "assets/image_01.png",
+                scale: 1,
+              ),
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            Search(),
+            SizedBox(
+              height: 30,
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
